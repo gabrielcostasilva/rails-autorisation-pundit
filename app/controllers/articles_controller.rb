c23b0380e816360892337d3adaa5594cdc3798f8
Gabriel Costa Silva <gabriel_costasilva@yahoo.com.br>
2024-10-10 10:45:50 -0300

    Ensured authorisation is requested

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
+  after_action :verify_authorized, except: [:index, :show]
 
   def index
     @articles = Article.all
